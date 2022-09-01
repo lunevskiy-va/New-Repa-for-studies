f40d7ed7f1a37164027beb848e397369b241c61f
lunevskiy-va <vikluntik@gmail.com>
2022-09-01 18:58:10 +0700

    Nasledovanie primer

---
+++ Studies/Studies/Nasledovanie.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class Student {
+public:
+	string name;
+	int group;
+	int age;
+	Student():Student{"Vitaly",1,18}{}
+	Student(string namep, int groupp, int agep) {
+		name = namep;
+		group = groupp;
+		age = agep;
+	}
+	int SetAge(int agep) {
+		age = agep;
+		return age;
+	}
+	int SetGroup(int groupp) {
+		group = groupp;
+		return group;
+	}
+	
+};
+
+class Aspirant :public Student {
+public:
+	int day_of_ex;
+	int grade;
+	Aspirant(int day_of_exp, int gradep) {
+		day_of_ex = day_of_exp;
+		grade = gradep;
+	}
+
+	void Show() {
+		cout << "Student is gropy " << group << " vozrastom " << age <<" let po imeni " << name << " dney do examena " << day_of_ex << " predvaritelnaya ocenka " << grade << endl;
+	}
+};
+
+int main() {
+	Aspirant Vitaly(15, 4);
+	Vitaly.Show();
+
+	return 0;
+}
