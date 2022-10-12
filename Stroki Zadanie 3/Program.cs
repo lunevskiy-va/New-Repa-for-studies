69de114649f5b42ba3806c85ca54f27467095a4e
lunevskiy-va <106383751+lunevskiy-va@users.noreply.github.com>
2022-10-12 21:11:46 +0700

    Stroki Zadanie 3
    3 zadanie neobhodimo dodelat*

---
+++ Stroki Zadanie 3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите текст с цифрами:  ");
+string text = Console.ReadLine();
+char[] Arr = text.ToCharArray();
+int[] Arr2=new int[Arr.Length];
+for (int i = 0; i < Arr.Length; i++)
+{
+    if (Arr[i] >= '1' && Arr[i] <= '9')
+    {
+        Arr2[i] = (int)Char.GetNumericValue(Arr[i]);     
+    }
+}
+Console.WriteLine(Arr2.Max());
+Console.WriteLine(Arr2.Sum());
+
+string text2 = Console.ReadLine();
+
+
+//Ввести второе слово и сравнить его посимвольно с первым текстом
