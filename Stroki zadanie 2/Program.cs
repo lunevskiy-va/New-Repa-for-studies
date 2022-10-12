c2bb8ad235b9c5221bc560057036ec62c8a7cb15
lunevskiy-va <106383751+lunevskiy-va@users.noreply.github.com>
2022-10-12 20:41:06 +0700

    Stroki zadanie 2
    Zadanie 2 v klasse na stroki

---
+++ Stroki zadanie 2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Введите слово с клавиатуры:  ");
+string Slovo=Console.ReadLine();
+Console.WriteLine("Вставляем Звездочки слева и справа равную длине введенного слова: ");
+int Dlina =Slovo.Length;
+Slovo=Slovo.PadLeft(Dlina+Slovo.Length,'*');
+int Dlina2=Slovo.Length;
+Slovo = Slovo.PadRight(Dlina+Slovo.Length, '*');
+Console.WriteLine(Slovo);
+Console.WriteLine("Заменяем * на # в строке: ");
+Slovo=Slovo.Replace('*', '#');
+Console.WriteLine(Slovo);
+Console.WriteLine("Вырезаем все # в строке: ");
+Slovo =Slovo.Replace("#", "");
+Console.WriteLine(Slovo);
+Console.WriteLine("Введите слово из двух символов с клавиатуры и сравниваем его встречается ли она в первом слове:  ");
+string Slovo_2=Console.ReadLine();
+
+if(Slovo.Contains(Slovo_2))
+{
+    Console.WriteLine(Slovo.IndexOf(Slovo_2));
+}
+else
+{
+    Console.WriteLine("Danger Danger");
+}
