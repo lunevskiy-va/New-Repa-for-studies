a3c5827e885d151be0749862b36398901ce023fa
lunevskiy-va <vikluntik@gmail.com>
2022-10-17 14:41:53 +0700

    DZ
    Stroki b Klassi

---
+++ DZ klassi/Program.cs
@@ -0,0 +1,73 @@
+﻿Student One=new Student("Иван","Добряк","Иванович","СТ-11",23);
+One.Print();
+
+
+
+class Student
+{
+    private string Name;
+    private string Family;
+    private string Surname;
+    private string Group;
+    private int Age;
+    public int[][] Myarr=new int [2][];
+    
+    public Student(string? pName,string? pFamily,string? pSurname,string? pGroup,int pAge, int[][] pMyarr)
+    {
+        Name= pName;
+        Family= pFamily;
+        Surname= pSurname;
+        Group= pGroup;
+        Age= pAge;
+        Myarr= pMyarr;
+    }
+
+    public string name
+    {
+        get { return Name; }
+        set { Name =value; }
+    }
+
+    public string family
+    {
+        get { return Family; }
+        set { Family = value; }
+    }
+
+    public string surname
+    {
+        get { return Surname; }
+        set { Surname = value; }
+    }
+
+    public int age
+    {
+        get { return Age; }
+        set { Age = value; }
+    }
+
+    public string group
+    {
+        get { return Group; }
+        set { Group = value; }
+    }
+
+    public void Zapol()
+    {
+        Console.WriteLine("Введите Имя: ");
+        Name = Console.ReadLine();
+        Console.WriteLine("Введите Фамилию: ");
+        Family = Console.ReadLine();
+        Console.WriteLine("Введите Отчество: ");
+        Surname = Console.ReadLine();
+        Console.WriteLine("Введите Возраст: ");
+        Age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите № группы: ");
+        Group = Console.ReadLine();
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Студент - {Family} {Name} {Surname} возрастом {Age} года, из группы № {Group}");
+    }
+
+}
---
+++ DZ stroki/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine("Введите текст из нескольких предложений, где разделителем будет (.)");
+string text = Console.ReadLine();
+int size=text.Length;
+char[] Arr=new char[size];
+text.CopyTo(0, Arr, 0, size);
+//Console.WriteLine(Arr);
+
+for (int i = 0; i < size; i++)
+{
+    if (i == 0)
+    {
+        Arr[i] = text.ToUpper()[i];
+    }
+
+    else if (Arr[i] == '.')
+    {
+        if (i == size-1)
+        {
+            break;
+        }
+        else
+        {
+            i++;
+            i++;
+            Arr[i] = text.ToUpper()[i];
+        }
+    }
+    else if (Arr[i] == '!')
+    {
+        if (i == size - 1)
+        {
+            break;
+        }
+        else
+        {
+            i++;
+            i++;
+            Arr[i] = text.ToUpper()[i];
+        }
+    }
+    else if (Arr[i] == '?')
+    {
+        if (i == size - 1)
+        {
+            break;
+        }
+        else
+        {
+            i++;
+            i++;
+            Arr[i] = text.ToUpper()[i];
+        }
+    }
+   
+}
+Console.WriteLine(Arr);
